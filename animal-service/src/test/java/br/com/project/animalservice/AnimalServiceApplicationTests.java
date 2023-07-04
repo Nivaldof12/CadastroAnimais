8a6c339593c41413a79cfea52daf5bdc8d3af3b0
Nivaldo <Nivaldo@DESKTOP-BI3T625>
2023-07-04 11:22:28 -0300

    First commit

---
+++ animal-service/src/test/java/br/com/project/animalservice/AnimalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.animalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
